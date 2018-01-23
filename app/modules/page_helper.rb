be1749650d79cccdb7f6a4304b98f6fbdad8f9a6
Xylophone <staff@xylophonexyz.com>
2018-01-22 17:32:42 -0800

    improve guessed title strategy

--- app/modules/page_helper.rb
+++ app/modules/page_helper.rb
@@ -3,20 +3,22 @@
 # Helper for Page related logic
 module PageHelper
   def guess_title(object)
-    title = object.title
-    text_helper = ActionController::Base.helpers
-    object.components.each do |collection|
-      media = collection.components.first.media unless collection.components.empty?
-      title = media if media.is_a?(String)
+    title = (object.title || object.description || '').strip
+    if title == ''
+      object.components.each do |collection|
+        media = collection.components.first.media unless collection.components.empty?
+        title = media if media.is_a?(String)
+      end
+      sorted_text_blacklist.each { |artifact| title = title.gsub(artifact, '') }
     end
-    text_helper.truncate(text_helper.strip_tags(title), length: 140)
+    ActionController::Base.helpers.truncate(ActionController::Base.helpers.strip_tags(title), length: 140).strip
   end
 
   def cover(object)
     photo = nil
     object.components.each do |collection|
       break if photo
-      collection.components.each { |component| photo = media_url(component) if media_component?(component) }
+      collection.components.each { |component| photo = media_url(component) if has_thumbnail?(component) }
     end
     photo
   rescue StandardError
@@ -24,6 +26,14 @@ module PageHelper
   end
 
   private
+
+  def sorted_text_blacklist
+    [/\\n/, /[^\w\s]/, /(opsinsert)/]
+  end
+
+  def has_thumbnail?(component)
+    component.is_a?(ImageComponent) || component.is_a?(VideoComponent)
+  end
 
   def media_component?(component)
     component.is_a?(ImageComponent) || component.is_a?(VideoComponent) ||
